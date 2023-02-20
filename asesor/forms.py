7e878ab97d8f1990866c49a2b907325445d6f131
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-02-20 13:13:40 -0500

    correción login

--- asesor/forms.py
+++ asesor/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class Login(forms.Form):
-    username = forms.EmailField(label='Usuario ', required=True)
+    username = forms.CharField(label='Usuario ', required=True)
     password = forms.CharField(label='Contraseña ', widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
      required=True)
