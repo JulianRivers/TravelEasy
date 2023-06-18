689c028d1bd4f3acb3ef405556dce73c8fbf7616
Julian Riveros <juliancamilorifo@ufps.edu.co>
2023-06-18 15:39:53 -0500

    Ver detalle de los eventos - Funciona botón ver más de evento

--- empleado/urls.py
+++ empleado/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name="empleado"
 urlpatterns = [
     path('inicio', views.inicio, name="inicio"),
+    path('evento/<int:idEvento>', views.evento, name="evento"),
+    path('evento/agregar', views.agregarViatico, name="evento"),
 ]
 
--- empleado/views.py
+++ empleado/views.py
@@ -1,16 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404)
 from django.contrib.auth.decorators import login_required
-from gerente.models import (Evento)
+from gerente.models import (Evento, Viatico)
+from django.http import JsonResponse
 # Controladores del empleado
 
 @login_required
 def inicio(request):
     """
-    root:inicio
+    Página de inicio donde el empleado ve todos los eventos a los que está asociado
+    name: "empleado:inicio"
     """
     user = request.user
     eventos = Evento.objects.filter(asistenciaevento__usuario=user)
     context = {
         'eventos': eventos
     }
-    return render(request, 'inicio.html', context)
+    return render(request, 'eventos.html', context)
+
+@login_required
+def evento(request, idEvento:int):
+    """
+    Página donde se ven los viaticos en detalle del evento
+    name = "empleado:evento"
+    """
+    evento = get_object_or_404(Evento, id=idEvento)
+    viaticos = Viatico.objects.filter(evento=evento)
+
+    context = {
+        'evento': evento,
+        'viaticos': viaticos
+    }
+    return render(request, 'eventos-detalle.html', context)
+
+@login_required
+def agregarViatico(request, idEvento:int):
+    """
+    Pagina donde se agrega la información de los viaticos
+    name= "empleado:inicio"
+    """
+    user = request.user
+    eventos = Evento.objects.filter(asistenciaevento__usuario=user)
+    context = {
+        'eventos': eventos
+    }
+    return JsonResponse({'status': 'success'})
+
